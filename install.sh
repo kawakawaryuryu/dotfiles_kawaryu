994209ac60f84d4233ca030b000f41ec7f397721
EC2 Default User <ec2-user@ip-172-31-20-194.ap-northeast-1.compute.internal>
2016-04-09 00:20:12 +0900

    escape .vim and .zprezto install

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 for file in .??*
 do
 	[ $file = ".git" ] && continue
+	[ $file = ".vim" ] && continue
+	[ $file = ".zprezto" ] && continue
 
 	ln -sf "$HOME/dotfiles/$file" "$HOME/$file"
 	#echo $file
